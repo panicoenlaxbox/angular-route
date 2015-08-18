dd72825fd66289132d8a58d069a1121e28d50a7b
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-08-18 19:32:00 +0200

    Navigation with

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,7 +1,9 @@
 (function() {
 	var app = angular.module("myApp");
-	app.controller("MainCtrl", ["$scope", function($scope) {
-		
+	app.controller("MainCtrl", ["$scope", "$location", function($scope, $location) {	
+		$scope.navigate = function() {
+			$location.path("/customers");
+		};
 	}]);
 	app.controller("OrdersCtrl", ["$scope", "$routeParams", function($scope, $routeParams) {
 		$scope.title = "Orders " + $routeParams.id;
